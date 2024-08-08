2d34bf2c299778cf7ab17aa1cf7e8389e6ce9bb0
MuhammadSalmanKhan333 <salmanmkhan2002@gmail.com>
2024-08-08 17:15:13 +0500

    some changes are done

--- src/components/NavbarItem.jsx
+++ src/components/NavbarItem.jsx
@@ -1,26 +1,32 @@
 "use client";
 
+import { Suspense } from "react"; // Import Suspense
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
 
 export default function NavbarItem({ title, param }) {
   const searchParams = useSearchParams();
   const genre = searchParams.get("genre");
+
   return (
     <div>
-      <Link
-        className={`hover:text-amber-600 font-semibold relative ${
-          genre === param ? "active" : ""
-        }`}
-        href={`/?genre=${param}`}
-      >
-        {title}
-        <span
-          className={`absolute left-0 top-[1.7rem] h-[3px] w-full  bg-amber-500 transition-transform duration-300 transform ${
-            genre === param ? "scale-x-100" : "scale-x-0"
+      <Suspense fallback={<div>Loading...</div>}>
+        {" "}
+        {/* Add the suspense boundary */}
+        <Link
+          className={`hover:text-amber-600 font-semibold relative ${
+            genre === param ? "active" : ""
           }`}
-        />
-      </Link>
+          href={`/?genre=${param}`}
+        >
+          {title}
+          <span
+            className={`absolute left-0 top-[1.7rem] h-[3px] w-full bg-amber-500 transition-transform duration-300 transform ${
+              genre === param ? "scale-x-100" : "scale-x-0"
+            }`}
+          />
+        </Link>
+      </Suspense>
     </div>
   );
 }
